b4461b493ea926509acfd88734d543d88205e59f
mohitkstartbit <mohitk@startbitsolutions.com>
2024-02-09 12:34:28 +0530

    updated code

---
+++ VeggieOnlineShoppingAppBlazorHybrid/VeggieApp.Hybrid/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace VeggieApp.Hybrid
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
