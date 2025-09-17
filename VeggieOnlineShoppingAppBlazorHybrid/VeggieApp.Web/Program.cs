d3fceaf96933481d96a84633f6f35ec2888ac3c3
mohitkstartbit <mohitk@startbitsolutions.com>
2025-09-17 14:50:12 +0530

    Improved the code and upgrade UI

--- VeggieOnlineShoppingAppBlazorHybrid/VeggieApp.Web/Program.cs
+++ VeggieOnlineShoppingAppBlazorHybrid/VeggieApp.Web/Program.cs
@@ -2,7 +2,10 @@ using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Radzen;
 using VeggieApp.DataSource.Data;
+using VeggieApp.DataSource.Service.AdminService;
 using VeggieApp.DataSource.Service.AuthenticationService;
 using VeggieApp.DataSource.Service.OrderService;
 using VeggieApp.DataSource.Service.ProductService;
@@ -16,13 +19,21 @@ builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddAuthorizationCore();
 builder.Services.AddSingleton<ProductStore>();
 /*office*/
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://192.168.1.2:5000") });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5000/") });
 /*home*/
 /*builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://192.168.1.9:5000") });*/
+// Pick the correct one based on where your actual provider class is
 builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthenticationStateProvider>();
+builder.Services.AddScoped<CustomAuthenticationStateProvider>();
 builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddScoped<IOrderService, OrderService>();
-
+builder.Services.AddScoped<IAdminService, AdminService>();
+builder.Services.AddScoped<NotificationService>();
+builder.Services.AddScoped<TooltipService>();
+builder.Services.AddScoped<DialogService>();
+builder.Services.AddScoped<ContextMenuService>();
+builder.Services.AddRadzenComponents();
+builder.Services.AddIdentityCore<IdentityUser>().AddRoles<IdentityRole>();
 
 await builder.Build().RunAsync();
